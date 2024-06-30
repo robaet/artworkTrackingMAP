36273c4fefe6a5e76457de86d3a4c132bdc22a7d
robaet <71385291+robaet@users.noreply.github.com>
2024-06-30 15:51:56 +0200

    why do i have to add a commit message

--- mud_server.py
+++ mud_server.py
@@ -13,12 +13,19 @@ mud_file_server_IP = "127.0.0.1:7000" # IP address of the MUD FIle server. Must 
 class Inventory:
     def __init__(self):
         self.devices = {}
+        self.device_objects = {}
 
     def store_mud(self, device_id, mud):
         self.devices[device_id] = mud
 
     def get_mud(self, device_id):
         return self.devices.get(device_id)
+    
+    def store_mud_object(self, device_id, mud_object):
+        self.device_objects[device_id] = mud_object
+
+    def get_mud_object(self, device_id):
+        return self.device_objects.get(device_id)
 
 inventory = Inventory()
 
@@ -41,6 +48,18 @@ def get_mud(mud_file_url, device_id):
         else:
             return jsonify({'error': 'MUD not found'}), 404
 
+def convert_json_to_object(mud_file):
+        data = json.load(mud_file)
+        return MudFileObject(
+            name=data['name'],
+            manufacturer=data['manufacturer'],
+            model=data['model'],
+            policy=data['policy'],
+            last_update=data['last_update'],
+            mud_url=data['mud_url'],
+            cache_validity=data['cache-validity']
+        )
+
 #Function to fetch MUD file from the MUD File server
 #todo periodically check for updates to the MUD file
 def get_mud_file(url, device_id):
@@ -58,7 +77,7 @@ def get_mud_file(url, device_id):
         response = requests.get(url, headers=headers, allow_redirects=True)
         if response.status_code == 200:
             inventory.store_mud(device_id, response.content)
-            validate_certificate() #todo: implement certificate validation
+            inventory.store_mud_object(device_id, convert_json_to_object(inventory.get_mud(device_id)))
             logging.info(f"MUD file retrieved successfully for device ID {device_id}")
 
         elif response.status_code >= 300 and response.status_code < 400:
@@ -73,22 +92,6 @@ def get_mud_file(url, device_id):
     except requests.RequestException as e:
         logging.error(f"An error occurred while fetching the MUD file for device ID {device_id}: {e}")
 
-
-def validate_certificate():
-    # Check if the certificate is valid
-    pass
-
-def convert_json_to_object(mud_file):
-        data = json.load(mud_file)
-        return MudFileObject(
-            name=data['name'],
-            manufacturer=data['manufacturer'],
-            model=data['model'],
-            policy=data['policy'],
-            last_update=data['last_update'],
-            mud_url=data['mud_url'],
-            cache_validity=data['cache-validity']
-        )
 
 
 if __name__ == '__main__':
