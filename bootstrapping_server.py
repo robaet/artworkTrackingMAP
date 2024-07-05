93c8d16d9678cf7e3d8d5da967f3bee35b7bfb1e
yvmeis <yves.meister@uzh.ch>
2024-07-05 14:15:10 +0200

    changes as discussed

--- bootstrapping_server.py
+++ bootstrapping_server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from rule_enforcement import translate_to_iptables, enforce_ip_table
 import requests
 
 app = Flask(__name__)
@@ -10,14 +11,17 @@ def is_valid_ip(ip_address):
     # check if the IP address is in the allowed set
     return ip_address in ALLOWED_IPS
 
-#Function to tell MUD server to retrieve a MUD file for a device
+#Function to tell server to retrieve a MUD file for a device
 #Send a request to the MUD server with a device ID and MUD file URL
 def search_mud_file(mud_file_url, device_id, mud_server_IP):
     url = f"{mud_server_IP}/mud/{device_id}?mud_file_url={mud_file_url}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            pass
+            if not verify_mud_file(response.content):
+                print(f"MUD file retrieved for device ID {device_id} is invalid.")
+            else:
+                enforce_ip_table(translate_to_iptables(parse_mud(response.content)))
         else:
             print(f"Failed to request MUD file retrieval for device ID {device_id}. HTTP status code: {response.status_code}")
     except requests.RequestException as e:
@@ -31,6 +35,27 @@ def retrieve_mud_file(device_id):
         return jsonify({'error': 'Unauthorized IP address'}), 403
     search_mud_file(mud_file_url, device_id, mud_server_IP)
     return jsonify({'message': 'MUD file retrieval request sent to MUD server'}), 200
+
+def parse_mud(mud):
+    policies = []
+    json_string = mud.decode('utf-8')
+    import json
+    json_data = json.loads(json_string)
+
+    for direction in ['inbound', 'outbound']:
+        for rule in json_data['mud']['policy']['acl'][direction]:
+            policy = {
+                'direction': direction,
+                'protocol': rule['protocol'],
+                'ports': rule['src-port'],
+                'action': rule['action']
+            }
+            policies.append(policy)
+    return policies
+
+def verify_mud_file(mud_file):
+    #todo implement MUD file verification
+    return True
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
--- mud_server.py
+++ mud_server.py
@@ -1,14 +1,8 @@
-from flask import Flask, request, jsonify
-import json
-import requests
-import logging
-import MudFileObject
-from rule_enforcement import translate_to_iptables, enforce_ip_table
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 ALLOWED_IPS = {'192.168.1.100', '10.0.0.1', '13.38.251.115', '127.0.0.1', '192.168.1.145'}
-mud_file_server_IP = "http://127.0.0.1:7000" # IP address of the MUD FIle server. Must be changed to the actual IP address of the MUD File server!!!
 
 #Class to store MUD file and policies
 class Inventory:
@@ -21,14 +15,38 @@ class Inventory:
 
     def get_mud(self, device_id):
         return self.devices.get(device_id)
-    
-    def store_mud_policies(self, device_id, mud_object):
-        self.device_policies[device_id] = mud_object
-
-    def get_mud_policies(self, device_id):
-        return self.device_policies.get(device_id)
 
 inventory = Inventory()
+
+# sample device MUD file in JSON format. This MUD file (if enforced correctly) would allow the IoT device to accept inbound HTTP and HTTPS
+device_mud = {
+        'name': 'Sample MUD Profile',
+        'manufacturer': 'Example',
+        'model': 'ABC123',
+        'cache-validity': 48,
+        'mud_url': "http://example.com/mud-files/ABC123",
+        'last_update': "2019-04-17T09:47:00+00:00",
+        'policy': {
+            'acl': {
+                'inbound': [
+                    {
+                        'name': 'allow_http',
+                        'protocol': 'tcp',
+                        'src-port': [80, 443],
+                        'action': 'allow'
+                    }
+                ],
+                'outbound': [
+                    {
+                        'name': 'allow_http',
+                        'protocol': 'tcp',
+                        'src-port': [80, 443],
+                        'action': 'allow'
+                    }
+                ]
+            }
+        }
+    }
 
 
 def is_valid_ip(ip_address):
@@ -36,79 +54,24 @@ def is_valid_ip(ip_address):
     return ip_address in ALLOWED_IPS
 
 #Endpoint to retrieve MUD file for a specific device
-#If the MUD file is not found in the inventory, the server will attempt to fetch it from the MUD File server
+#If the MUD file is not found in the inventory, the server just use the sample device MUD file for now
 @app.route('/mud/<device_id>', methods=['GET'])
 def retrieve_mud(device_id):
+    #TODO do something with the MUD file url. So far the mudfiles are retrieved via device ID
     mud_file_url = request.args.get('mud_file_url')
     mud = inventory.get_mud(device_id)
     if mud:
+        print(f"MUD file found for device ID {device_id}")
         return jsonify({'mud': mud}), 200
     else:
-        get_mud_file(mud_file_url, device_id)
-        mud = inventory.get_mud(device_id)
-        ip_table = translate_to_iptables(inventory.get_mud_policies(device_id))
-        enforce_ip_table(ip_table)
-        if mud:
-            return jsonify({'message': "MUD found"}), 200
-        else:
-            return jsonify({'error': 'MUD not found'}), 404
-
-#Function to convert JSON data to MudFileObject
-def convert_json_to_object(mud_file):
-        data = json.load(mud_file)
-        return MudFileObject(
-            name=data['name'],
-            manufacturer=data['manufacturer'],
-            model=data['model'],
-            policy=data['policy'],
-            last_update=data['last_update'],
-            mud_url=data['mud_url'],
-            cache_validity=data['cache-validity'],
-            inbound=data['policy']['acl']['inbound'],
-            outbound=data['policy']['acl']['outbound']
-        )
-
-#Function to parse MUD file and extract policies
-def parse_mud(mud):
-    policies = []
-    json_string = mud.decode('utf-8')
-    import json
-    json_data = json.loads(json_string)
-
-    for direction in ['inbound', 'outbound']:
-        for rule in json_data['mud']['policy']['acl'][direction]:
-            policy = {
-                'direction': direction,
-                'protocol': rule['protocol'],
-                'ports': rule['src-port'],
-                'action': rule['action']
-            }
-            policies.append(policy)
-    return policies
-
-
-
-#Function to fetch MUD file from the MUD File server
-#todo periodically check for updates to the MUD file
-def get_mud_file(url, device_id):
-    url=url + "/mud"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            inventory.store_mud(device_id, response.content)
-            if not verify_mud_file(response.content):
-                print(f"MUD file retrieved for device ID {device_id} is invalid.")
-                return
-            inventory.store_mud_policies(device_id, parse_mud(inventory.get_mud(device_id)))
-            print(f"MUD file retrieved successfully for device ID {device_id}")
-        else:
-            print(f"Failed to retrieve MUD file for device ID {device_id}. HTTP status code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"An error occurred while fetching the MUD file for device ID {device_id}: {e}")
-
-def verify_mud_file(mud_file):
-    #todo implement MUD file verification
-    return True
+        inventory.store_mud(device_id, device_mud)
+        print(f"sample MUD file used for device ID {device_id}")
+        return jsonify({'mud': device_mud}), 200
+        
+#TODO implement a POST endpoint to add new MUD files from the outside
+@app.route('/mud/<device_id>', methods=['POST'])
+def post_mud(device_id):
+    pass
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
