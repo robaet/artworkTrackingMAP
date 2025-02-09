b5885608c77b4cfea6339b508fb9de46a48d43dc
yvmeis <yves.meister@uzh.ch>
2025-02-09 15:04:25 +0100

    change

--- bootstrapping_server.py
+++ bootstrapping_server.py
@@ -2,13 +2,10 @@ from flask import Flask, request, jsonify
 from rule_enforcement import translate_to_iptables, enforce_ip_table
 import requests
 import json
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.backends import default_backend
 from another_key_generator import verify_signature
 from apscheduler.schedulers.background import BackgroundScheduler
 import atexit
+import socket
 
 app = Flask(__name__)
 class Inventory:
@@ -27,8 +24,8 @@ ALLOWED_IPS = {'192.168.1.100', '10.0.0.1', '13.38.251.115', '127.0.0.1', '192.1
 
 def is_valid_ip(ip_address):
     # check if the IP address is in the allowed set
-    return ip_address in ALLOWED_IPS
-    # return True #For testing purposes, allow all IP addresses
+    # return ip_address in ALLOWED_IPS
+    return True #For testing purposes, allow all IP addresses
 
 #Function to tell server to retrieve a MUD file for a device
 #Send a request to the MUD server with a device ID, also verifies the MUD file
@@ -86,13 +83,24 @@ def log_sensor_data1():
 @app.route('/', methods=['POST'])
 def log_sensor_data2():
     print(f"Received POST request from Board")
-    sensor_data = request.form.get('data')
-    print(f"Received data from Board: {sensor_data}")
-    if sensor_data:
+    request.data = request.data.decode('utf-8')
+    request.data = json.loads(request.data)
+    print(f"Received data from Board: {request.data}")
+    time_data = request.data.get('time')
+    temperature_data = request.data.get('temperature')
+    humidity_data = request.data.get('humidity')
+    accel_data = request.data.get('acceleration')
+    print(f"Received time data from Board: {time_data}")
+    print(f"Received temperature data from Board: {temperature_data}")
+    print(f"Received humidity data from Board: {humidity_data}")
+    print(f"Received acceleration data from Board: {accel_data}")
+    if time_data and temperature_data and humidity_data and accel_data:
         with open('sensor_data.txt', 'a') as file:
-            file.write(sensor_data + '\n')
-            print(f"Wrote data to sensor_data.txt")
-        return "Data logged successfully.", 200
+            file.write(time_data + '!' + temperature_data + '\n')
+            file.write(time_data + '!' + humidity_data + '\n')
+            file.write(time_data + '!' + accel_data + '\n')
+            print(f"Wrote all data to sensor_data.txt")
+        return "All data logged successfully.", 200
     else:
         return "No data provided.", 400
 
@@ -120,8 +128,32 @@ def updatePolicies():
         else:
             print(f"Failed to update policies for device ID {device_id}. HTTP status code: {response.status_code}")
 
+def dataHandlerSocket():
+    print("Starting data handler server...")
+    HOST = '0.0.0.0'
+    PORT = 5000
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((HOST, PORT))
+            server_socket.listen(5) # 5 connections
+
+            print(f"Server listening on {HOST}:{PORT}")
+
+            while True:
+                conn, addr = server_socket.accept()
+                with conn:
+                    print('Connected by', addr)
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    print('Received', repr(data))
+                    conn.sendall(data)
+    except KeyboardInterrupt:
+        print("\nServer stopped by user.")
 
 if __name__ == '__main__':
+    print("Starting bootstrapping server...")
+    dataHandlerSocket()
     scheduler = BackgroundScheduler()
     job = scheduler.add_job(updatePolicies, 'interval', seconds=10)
     scheduler.start()
